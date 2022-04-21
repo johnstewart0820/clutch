37b92ab8b46ec3b733eb8ed4bfaf41562dd546de
DevJohn <jhon.stewart0820@gmail.com>
2022-04-21 14:11:49 -0400

    add: build

---
+++ static/js/5.c7df9b32.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpclutch-web-app"]=this["webpackJsonpclutch-web-app"]||[]).push([[5],{1956:function(p,c){},1970:function(p,c){}}]);
+//# sourceMappingURL=5.c7df9b32.chunk.js.map
---
+++ static/js/7.bb4bec71.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpclutch-web-app"]=this["webpackJsonpclutch-web-app"]||[]).push([[7],{1922:function(p,c){}}]);
+//# sourceMappingURL=7.bb4bec71.chunk.js.map
