8ff3a8144a9f8739e62d2dce448289297b05d5a3
DevJohn <jhon.stewart0820@gmail.com>
2022-06-17 03:19:58 -0400

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8282ffab3a915c2b743ace614d6750c8.js"
+  "/precache-manifest.15ef57a6f849d4adf050fa5bf37b3a53.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ static/js/21.d612fc4b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpclutch-web-app"]=this["webpackJsonpclutch-web-app"]||[]).push([[21],{2042:function(p,c){},2056:function(p,c){}}]);
+//# sourceMappingURL=21.d612fc4b.chunk.js.map
